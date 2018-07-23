d33f45673b80165e9fad157eb4bde9fabb99da38
ganesh chukka <40388511+ganeshchukka@users.noreply.github.com>
2018-07-23 15:02:45 +0530

    Delete httpClient.service.ts

--- services/Httpclient/httpClient.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-@Injectable()
-export class HttpClientService {
-constructor (private _http: HttpClient) { }
-public getCountries(): any {
-return this._http.get('http://restcountries.eu/rest/v2/all');
-}
-}
