adc451f5c4af065c09a57cca9e02b0abcc010496
ganesh chukka <40388511+ganeshchukka@users.noreply.github.com>
2018-07-23 15:02:56 +0530

    Delete httpclient2.service.ts

--- services/Httpclient/httpclient2.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-@Injectable()
-export class HttpClientTwoService {
-constructor (private _http: HttpClient) { }
-public convertToUpperCase(obj): any {
-    return this._http.post('http://test-routes.herokuapp.com/test/uppercase', obj);
-}
-}
